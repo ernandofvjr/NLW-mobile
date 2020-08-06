acd5aa61eed26f37aca2d59a9e62be2aebf396f8
Ernando <ernandofvjr@hotmail.com>
2020-08-06 18:52:33 -0300

    Adicionar landing page

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Image, Text, TouchableOpacity} from 'react-native';
+
+import styles from './styles';
+
+function GiveClasses() {
+    return (
+        <View style={styles.container}>
+        </View>
+    );
+}
+
+export default GiveClasses;
