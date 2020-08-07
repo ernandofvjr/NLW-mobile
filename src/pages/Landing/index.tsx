ff5d4419e034f0467971dbed2204045d1095883d
Ernando <ernandofvjr@hotmail.com>
2020-08-07 20:43:10 -0300

    Adiciona page header e navegação

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import {View, Image, Text, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
-import giveclassesIcon from '../../assets/images/icons/give-classes.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
+import styles from './styles';
+
 function Landing() {
-    const navigation = useNavigation();
+    const { navigate } = useNavigation();
 
     function handleNavigateToGiveClassesPage() {
-
-    };
+        navigate('GiveClasses')
+    }
+    
+    function handleNavigateToStudyPages() {
+        navigate('Study')
+    }
 
     return (
         <View style={styles.container}>
@@ -28,15 +34,19 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    onPress={handleNavigateToStudyPages}
+                    style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon}></Image>
                 <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
-                    <Image source={giveclassesIcon}></Image>
+                <RectButton 
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon}></Image>
                 <Text style={styles.buttonText}>Dar aulas</Text>
-                </TouchableOpacity>
+                </RectButton>
 
             </View>
 
